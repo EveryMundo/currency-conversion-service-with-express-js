c4a680c71e33742e64d35e04add469849b488336
Daniel Santana <github@danielsantana.net>
2018-03-20 18:30:57 -0400

    adds APP_SEC_PORT in case the application uses it

--- config.js
+++ config.js
@@ -6,14 +6,16 @@ const micro  = require('microtime');
 const ip     = require('ip');
 
 const defaults = {
-  APP_PORT: 8907,
+  APP_PORT: ~~(Math.random() * 64000), // eslint-disable-line no-bitwise
+  APP_SEC_PORT: ~~(Math.random() * 64000), // eslint-disable-line no-bitwise
   APP_IP: '0.0.0.0',
   LOG_LEVEL: 'info',
 };
 
 const isValidLogLevel = level => 'trace,debug,info,warn,error,fatal'.split(',').includes(level);
 
-const APP_PORT = Math.abs(env.APP_PORT) || defaults.APP_PORT;
+const APP_PORT     = Math.abs(env.APP_PORT)     || defaults.APP_PORT;
+const APP_SEC_PORT = Math.abs(env.APP_SEC_PORT) || defaults.APP_SEC_PORT;
 
 const APP_IP = ip.isV4Format(env.APP_IP) ? env.APP_IP : defaults.APP_IP;
 
@@ -21,6 +23,7 @@ const LOG_LEVEL = isValidLogLevel(env.LOG_LEVEL) ? env.LOG_LEVEL : defaults.LOG_
 
 const config = {
   APP_PORT,
+  APP_SEC_PORT,
   APP_IP,
   LOG_LEVEL,
   NUM_OF_WORKERS: Math.abs(env.NUM_OF_WORKERS),
@@ -36,7 +39,7 @@ const config = {
       ipAddr:     env.EUREKA_APP_IP_ADDR,
       // statusPageUrl,
       port:       APP_PORT,
-      securePort: env.EUREKA_APP_SECURE_PORT,
+      securePort: APP_SEC_PORT,
       vipAddress: env.EUREKA_VIP_ADDRESS,
     },
     host: env.EUREKA_HOST,
